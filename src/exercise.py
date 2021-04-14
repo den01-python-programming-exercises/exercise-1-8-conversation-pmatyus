c787a8ae0b47ecbe1b774b3ea893b02f6cae059c
pmatyus <>
2021-04-14 19:22:40 +0000

    Changed print function to input functions

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,7 @@
 def main():
     #write your code below this line
-
+  input('Greetings! How are you doing?')
+  input('Oh, how interesting. Tell me more!')
+  print('Thanks for sharing!')
 if __name__ == '__main__':
     main()
